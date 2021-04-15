bb394270baa720dc5b5b5ac0080c6ffcfac3db3a
nickpythonluajava <78268270+nickpythonluajava@users.noreply.github.com>
2021-04-15 09:54:02 -0400

    change to actually darn work

---
+++ garfield randomizer.py
@@ -0,0 +1,10 @@
+import webbrowser as web
+import time
+import random as math
+y = math.randint(1978,2021)
+m = math.randint(1,12)
+d = math.randint(1,31)
+
+
+web.open('https://www.gocomics.com/garfield/' , y , '/' , m , '/' , d)
+time.sleep(99999)
